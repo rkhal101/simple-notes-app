febfd516bb54f63269622e7b352cf89b263e149b
oscp <oscp@Ranas-MacBook-Air.local>
2019-05-19 15:14:13 -0400

    commit

---
+++ notes.js
@@ -0,0 +1,106 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// const getNotes = function() {
+//     return 'These are my notes...'
+// }
+
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    
+    const duplicateNote = notes.filter(
+        (note) => note.title == title)
+    
+
+    if (!duplicateNote){
+        notes.push({
+            title: title,
+            body: body
+        })
+        console.log(chalk.green.inverse('New note added!'))
+        saveNotes(notes)
+
+    } else {
+        console.log(chalk.red.inverse('Title already taken!'))
+    }
+
+}
+
+const loadNotes = () => {
+
+    try {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
+    } catch (e){
+        return []
+    }
+
+
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title != title
+    )
+
+    if(notes.length == notesToKeep.length){
+        console.log(chalk.red.inverse("No note removed!"))
+
+    }
+    else {
+        console.log(chalk.green.inverse("Note removed!"))
+    }
+
+    saveNotes(notesToKeep)
+
+}
+
+const listNotes = () => {
+    console.log(chalk.underline.magenta("Titles of Notes"))
+    const notes = loadNotes()
+    notes.forEach(element => {
+        console.log(element.title)
+        
+    });
+}
+
+const readNote = (title) => {
+
+    const notes = loadNotes()
+    const notesToRead = notes.filter((note) => note.title == title
+    )
+
+    if (notesToRead.length == 0) {
+        
+        console.log(chalk.red("No note with the provided title exists."))
+    } else {
+
+        notesToRead.forEach( note => console.log(chalk.magenta.underline(note.title)))
+        notesToRead.forEach( note => console.log(note.body))
+        
+    }
+
+    // console.log(chalk.underline.magenta("Titles of Notes"))
+    // const notes = loadNotes()
+    // notes.forEach(element => {
+    //     console.log(element.title)
+        
+    // });
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+
+}
+
+
+
+module.exports = {
+    //getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
